72efdfbb891f7a62e3f53f2515e97af32284e395
minjeong <mjkimdelta@gmail.com>
2021-03-03 02:55:52 +0900

    static 파일 불러오기 에러

--- Scripts/dramarama/urls.py
+++ Scripts/dramarama/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from django.conf.urls import include, url
 from . import views
 
+
 # app_name = 'dramarama'
 urlpatterns = [
     path('', views.index, name='index'),
--- Scripts/mysite/settings.py
+++ Scripts/mysite/settings.py
@@ -30,14 +30,13 @@ ALLOWED_HOSTS = ['127.0.0.1', 'localhost',]
 # Application definition
 
 INSTALLED_APPS = [
-    'dramarama.apps.DramaramaConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    #'dramarama',
+    'dramarama',
 ]
 
 MIDDLEWARE = [
@@ -118,8 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = '/dramarama/static/'
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, '/dramarama/static'),
-)
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
